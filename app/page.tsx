ecec22783bea2a93d6f80ba17321ea622bf87e13
carldouch <goldstraitjacket@gmail.com>
2025-03-28 09:59:19 +0100

    fix padding top3

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Logo from "./Logo";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen font-[family-name:var(--font-geist-sans)]">
+    <div className="flex justify-center min-h-screen font-[family-name:var(--font-geist-sans)]">
       <div className="pt-3">
         <Logo />
       </div>
